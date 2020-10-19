3981b784226dd392032739b33b10413e5cc039aa
arthurmvr <73127771+arthurmvr@users.noreply.github.com>
2020-10-19 19:58:08 -0300

    Update Engenheiro.cpp

--- Engenheiro.cpp
+++ Engenheiro.cpp
@@ -7,6 +7,10 @@ class Engenheiro : public Empregado {
     std::string nome;  
 	int projetos;
   void Imprimir() override {
-	  
+  	 std::cout << "Nome: " << nome << std::endl;
+ 	 std::cout << "Salario Mes: " << eng1.pagamentoMes(9.5) << std::endl;
+ 	 std::cout << "Projetos: " << eng1.projetos << std::endl;
+ 	 std::cout << std::endl;
+  }
 };
 
