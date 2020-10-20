eb724433105047836a861170b914c542b4b5e1ed
arthurmvr <73127771+arthurmvr@users.noreply.github.com>
2020-10-20 10:44:38 -0300

    Update Engenheiro.cpp

--- Engenheiro.cpp
+++ Engenheiro.cpp
@@ -4,13 +4,22 @@
 class Engenheiro : public Empregado {
 
   public:
-    std::string nome;  
-	int projetos;
-  void Imprimir(double horasTrabalhadas) override {
-  	 std::cout << "Nome: " << nome << std::endl;
- 	 std::cout << "Salario Mes: " << pagamentoMes(horasTrabalhadas) << std::endl;
- 	 std::cout << "Projetos: " << projetos << std::endl;
- 	 std::cout << std::endl;
-  }
-};
 
+    Engenheiro(std::string _nome, double _salarioHora, int _projetos){
+     nome = _nome;
+     salarioHora = _salarioHora;
+     projetos = _projetos;
+    }
+
+    void Imprimir(double horasTrabalhadas){
+      std::cout << "Nome: " << nome << std::endl;
+      std::cout << "Salario Mes: " << pagamentoMes(horasTrabalhadas) << std::endl;
+      std::cout << "Projetos: " << projetos << std::endl;
+      std::cout << std::endl;
+    }
+    
+  private:
+    std::string nome;
+    int projetos;
+
+};
