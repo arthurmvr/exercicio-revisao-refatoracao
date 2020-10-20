786928cce45205793f8a112b741b92c23f29738f
arthurmvr <73127771+arthurmvr@users.noreply.github.com>
2020-10-20 10:44:05 -0300

    Update Vendedor.cpp

--- Vendedor.cpp
+++ Vendedor.cpp
@@ -1,15 +1,28 @@
 #include <string>
 #include "Empregado.hpp"
+const int MESES = 12;
 
 class Vendedor : public Empregado {
 
   public:
-    const int MESES = 12;
-    std::string nome;  	  
-	
-	double quotaTotalAnual() {
-	  return quotaMensalVendas * MESES;
-	}
-	
-};
+    Vendedor(std::string _nome, double _salarioHora, double _quotaMensalVendas){
+      nome = _nome;
+      salarioHora = _salarioHora;
+      quotaMensalVendas = _quotaMensalVendas;
+    }
 
+	  double quotaTotalAnual() {
+	    return quotaMensalVendas * MESES;
+	  }
+
+	  void Imprimir(double horasTrabalhadas){
+      std::cout << "Nome: " << nome << std::endl;
+      std::cout << "Salario Mes: " << pagamentoMes(horasTrabalhadas) << std::endl;
+      std::cout << "Quota vendas: " << quotaTotalAnual() << std::endl;
+      std::cout << std::endl;
+	  }
+
+  private:
+    std::string nome;
+  
+};
